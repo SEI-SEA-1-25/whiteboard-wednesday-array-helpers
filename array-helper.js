3a52827be9dbb8f63ecb7f3d5f22e0e599f8ad66
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-04-07 08:56:25 -0700

    solution

---
+++ array-helper.js
@@ -0,0 +1,28 @@
+// class Array {
+//     constructor()
+// }
+
+Array.prototype.sum = function() {
+    let total = 0 
+    this.forEach(item =>{
+        total += item
+        // console.log(item)
+        // console.log(total)
+    })
+    return total
+    // console.log(total)
+}
+let numbers = [5, 10, 3, 6];
+let sumOfNumbers = numbers.sum()
+console.log(sumOfNumbers)
+
+Array.prototype.average = function() {
+    let sum = this.sum()
+    let count = 0
+    this.forEach(item => {
+        count += 1
+    })
+    return (sum/count)
+}
+let averageOfNumbers = numbers.average()
+console.log(averageOfNumbers) 
